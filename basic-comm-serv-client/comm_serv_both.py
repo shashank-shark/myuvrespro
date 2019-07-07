c466deede9f5eb6c4ac984a51351a4160b11446e
shashank-shark <shashankreddy231@gmail.com>
2019-07-07 23:14:43 +0530

    Added class comm server

--- basic-comm-serv-client/comm_serv_both.py
+++ basic-comm-serv-client/comm_serv_both.py
@@ -11,12 +11,15 @@ SERVER_HOST = 'localhost'
 COMMUNICATION_SERVER_NAME = 'server'
 
 # we will define some of the common utilities for the processess
+#---------------------------------------------------------------
 def send (channel, *args):
     buffer = cPickle.dumps(args)
     value = socket.htonl(len(buffer))
     size = struct.pack("L", value)
     channel.send(size)
     channel.send(buffer)
+
+#----------------------------------------------------------------
 
 def recieve (channel):
     size = struct.calcsize ("L")
@@ -32,3 +35,42 @@ def recieve (channel):
         buf = channel.recv(size - len(buf))
 
     return cPickle.loads(buf)[0]
+
+#-----------------------------------------------------------------
+
+# communication server class here
+class CommServer (object):
+    """ This communication server is built on select """
+    def __init__ (self, port, backlog=5):
+        self.clients = 0
+        self.clientmap = {}
+        self.outputs = []   # to list output sockets
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # enable reusing the socket address
+        self.server.setsocketop(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind ((SERVER_HOST, port))
+        print ('Server Listening to port : ', port)
+        self.server.listen(backlog)
+
+        # for catching the keyboard interrupts
+        signal.signal(signal.SIGINT, self.sighandler)
+
+        def sighandler (self, signum, frame):
+            """ Clean up the client outputs """
+            # close the server
+            print ('Shutting down the server')
+
+            # close existing client sockets
+            for output in self.outputs:
+                output.close()
+            
+            self.server.close()
+        
+        def get_client_name (self, client):
+            """ Return the name of the client """
+            info = self.clientmap[client]
+            host, name = info[0][0], info[1]
+            return '@'.join((name, host))
+
